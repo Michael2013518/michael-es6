efbbde08abe72cfa9540e7b1bde90d3c29c00c01
Michael <zdong2010@gmail.com>
2018-08-24 07:42:16 +0800

    '添加script.js的demo文件'

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const obj = {
   greeting:'hello ~',
   name:'Michael Project'
@@ -5,3 +7,22 @@ const obj = {
 
 const { greeting, name} = obj
 console.log(greeting, name)
+
+function breakfast(){
+  return ['蛋糕','奶茶','苹果'];
+}
+// 解构数组和对象
+var tmp = breakfast(),
+    dessert = tmp[0],drink = tmp[1],fruit = tmp[2];
+console.log(dessert,drink,fruit);
+
+let Dessert ='蛋糕',
+    Drink = '奶茶';
+
+let Breakfast = kitchen`今天的早餐是
+${Dessert} 与 ${Drink}!`;
+
+function kitchen (string,...value){
+console.log(string);
+console.log(value);
+}
